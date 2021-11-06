2f4c4302cbc85a9ae87928a8ca1c009c3e47d408
johnsaigle <john.saigle@gmail.com>
2021-11-06 17:37:34 -0400

    new-deb: Add set -e flag; install seclists

--- provision-new-deb.sh
+++ provision-new-deb.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
+# Run this script on a new Debian machine in order to get busy hacking.
+set -e
 
-# Idea: drop this on a new Debian VM and get going
 sudo apt update && sudo apt upgrade
 
 # Install golang via Google repo instead. The apt version seems broken (doesn't
 # support all operation modes).
 #sudo apt install gobuster
 sudo apt install nmap
+
+mkdir $HOME/wordlists
+git clone https://github.com/danielmiessler/SecLists.git $HOME/wordlists
