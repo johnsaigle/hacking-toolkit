d5387863ce92e3c24a30b538da7d4a37eac16534
johnsaigle <john.saigle@gmail.com>
2021-12-08 19:51:12 -0500

    Change paths to be more kali-like; remove tools I don't really use

--- provision-new-deb.sh
+++ provision-new-deb.sh
@@ -2,29 +2,25 @@
 # Run this script on a new Debian machine in order to get busy hacking.
 set -e
 
-sudo apt update && sudo apt upgrade
+apt update -y && apt upgrade -y
+apt install sudo
+apt install git
+apt install python3-pip -y
 
-sudo apt install python3-pip y
+pip install trufflehog3
 
 # Install golang via Google repo instead. The apt version seems broken (doesn't
 # support all operation modes).
 #sudo apt install gobuster
-sudo apt install nmap
+apt install nmap
 
 # Git tools:
-#   - GraphQL Map
-#   - jwt_tool
 #   - SQLmap
-#   - XXEInjector
 
 cd /opt/
-sudo git clone https://github.com/securing/DumpsterDiver.git
-sudo git clone https://github.com/sqlmapproject/sqlmap.git
-sudo git clone https://github.com/swisskyrepo/GraphQLmap.git
-sudo git clone https://github.com/ticarpi/jwt_tool.git
-sudo git clone https://github.com/enjoiz/XXEinjector.git
+git clone https://github.com/sqlmapproject/sqlmap.git
 
 # Wordlists
-mkdir $HOME/wordlists
-git clone https://github.com/danielmiessler/SecLists.git $HOME/wordlists
-
+mkdir -p /usr/share/wordlists/
+cd /usr/share/wordlists/
+git clone https://github.com/danielmiessler/SecLists.git
