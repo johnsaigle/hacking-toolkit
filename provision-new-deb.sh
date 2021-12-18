69c87c66b70d336f86cf05778d37c07284c4aecb
John <jsaigle@protonmail.com>
2021-12-18 11:52:06 -0500

    add docker-compose

--- provision-new-deb.sh
+++ provision-new-deb.sh
@@ -14,6 +14,10 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
 apt -y install docker-ce docker-ce-cli containerd.io
+# Install docker-compose. NOTE: Update version number
+# https://www.digitalocean.com/community/tutorials/how-to-install-docker-compose-on-debian-10
+curl -L https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 # Install Go
 cd /tmp/
